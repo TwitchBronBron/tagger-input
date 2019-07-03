11904a4e068a55d90d5842fc21c0e3acba201f84
Bronley Plumb <bronley@gmail.com>
2019-07-02 22:23:58 -0400

    Initial project add. Not working yet.

---
+++ index.js
@@ -0,0 +1,62 @@
+class TaggerInputElement extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        this.addEventListener('click', () => {
+            this.shadowRoot.getElementById('input').focus();
+        });
+        this.shadowRoot.innerHTML = `
+            <style>
+                :host {
+                    box-sizing: border-box;
+                    display: block;
+                    border: 1px solid rgb(238, 238, 238);
+                    border-style: inset;
+                    cursor: text;
+                }
+                :host:focus{
+                    outline: -webkit-focus-ring-color auto 1px;
+                }
+                #input {
+                    border: 0;
+                    outline: none;
+                    display: inline-block;
+                    min-width: 30px; 
+                    max-width: 400px;
+                }
+                .tag {
+                    display: inline-flex;
+                    align-content: center;
+                    justify-content: center;
+                    min-width: 0;
+                    padding-left: 4px;
+                    padding-right: 4px;
+                    border-style: solid;
+                    border-width: 1px;
+                    border-radius: 3px;
+                    font-size: 12px;
+                    line-height: 1.84615385;
+                    text-decoration: none;
+                    vertical-align: middle;
+                    white-space: nowrap;
+                    border-color: transparent;
+                    background-color: #E1ECF4;
+                    color: #2c5777;
+                    margin: 2px;
+                    font-size: 12px;
+                }
+            </style>
+            <span id="tag-container">
+                <span class="tag">Angular</span>
+                <span class="tag">JSX</span>
+            </span>
+            <span id="input" contenteditable="true"></span>
+
+        `;
+    }
+}
+
+window.customElements.define('tagger-input', TaggerInputElement);
